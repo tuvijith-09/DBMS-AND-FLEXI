1b7c8a05d71152d9fa7e2287d1ebbfd23adee487
tuvijith-09 <tuvijithofficial@gmail.com>
2026-04-04 02:11:59 +0530

    Delete src directory

--- src/service/CRUDOperations.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-public interface CRUDOperations<T> {
-    // Returns true if successful, false if there is a database error
-    boolean add(T obj); 
-    void delete(int id);
-    void viewAll();
-}
